a0eeec8ac41d77ea31ba3f036d6539f63568597d
Nick Webster <nick@nick.geek.nz>
2021-12-14 20:07:31 +1300

    Part 2

--- 2021/Rust/day14/src/main.rs
+++ 2021/Rust/day14/src/main.rs
@@ -1,51 +1,71 @@
 use std::collections::HashMap;
 use itertools::Itertools;
 
+type Pairs = HashMap<(char, char), u64>;
+
 fn main() {
     let raw = std::fs::read_to_string("input.txt").unwrap();
 
     let (template, rules) = raw.split_once("\n\n").unwrap();
+
     let rules = rules.lines()
         .map(|rule| rule.split_once(" -> ").unwrap())
-        .map(|(from, to)| (from.to_string(), to.to_string()))
-        .collect::<HashMap<String, String>>();
+        .map(|(from, to)| {
+            let chs = from.chars().collect_vec();
+            ((chs[0], chs[1]), to.chars().exactly_one().unwrap())
+        })
+        .collect::<HashMap<_, _>>();
 
-    let mut res = template.to_string();
+    let chars = template.chars().collect::<Vec<_>>();
+    let mut pairs = HashMap::<(char, char), u64>::new();
+    let mut last = '\0';
+    for i in 0..(chars.len() - 1) {
+        let pair = (chars[i], chars[i + 1]);
+        let old = if let Some(count) = pairs.get(&pair) { *count } else { 0 };
+        pairs.insert(pair, old + 1);
+        last = chars[i + 1];
+    }
+
+    let mut res = pairs;
     for _ in 0..40 {
-        res = step(&*res, &rules);
+        res = step(&res, &rules);
     }
 
-    let mut counts = HashMap::new();
-    for ch in res.chars() {
-        let old = if let Some(count) = counts.get(&ch) { *count } else { 0 };
-        counts.insert(ch, old + 1);
+    let mut commons = HashMap::new();
+    for ((a, b), count) in res {
+        let old = if let Some(count) = commons.get(&a) { *count } else { 0 };
+        commons.insert(a, old + count);
+        last = b;
     }
+    let old = if let Some(count) = commons.get(&last) { *count } else { 0 };
+    commons.insert(last, old + 1);
 
-    let (min_ch, min_count) = counts.iter().min_by_key(|(ch, count)| **count).unwrap();
-    let (max_ch, max_count) = counts.iter().max_by_key(|(ch, count)| **count).unwrap();
+    let (min_ch, min_count) = commons.iter().min_by_key(|(ch, count)| **count).unwrap();
+    let (max_ch, max_count) = commons.iter().max_by_key(|(ch, count)| **count).unwrap();
 
-    // println!("{:?}", res);
+    println!("{:?}", commons);
     println!("{:?}", max_count - min_count);
 }
 
-fn step(poly: &str, rules: &HashMap<String, String>) -> String {
-    let mut res = String::from(poly);
+fn step(pairs: &Pairs, rules: &HashMap<(char, char), char>) -> Pairs {
+    let mut res = HashMap::new();
 
-    let chars = poly.chars().collect::<Vec<_>>();
-    let mut offset = 0;
-    for i in 0..(chars.len() - 1) {
-        let pair = format!("{}{}", chars[i], chars[i + 1]);
-        if let Some(to) = rules.get(&*pair) {
-            res.insert_str(i + 1 + offset, &*to);
-            offset += 1;
-            // res.push_str(&*to);
+    for (pair, total) in pairs {
+        if let Some(to) = rules.get(pair) {
+            let (a, b) = *pair;
+            let total = *total;
+            let to = *to;
+
+            let new_p_l = (a, to);
+            let old_l = if let Some(count) = res.get(&new_p_l) { *count } else { 0 };
+            let new_p_r = (to, b);
+            let old_r = if let Some(count) = res.get(&new_p_r) { *count } else { 0 };
+            res.insert(new_p_l, old_l + total);
+            res.insert(new_p_r, old_r + total);
+        } else {
+            panic!("at the disco")
         }
     }
-    // for (from, to) in rules {
-    //     if poly.contains(&*from) {
-    //         res.push_str(&*to);
-    //     }
-    // }
 
     res
 }
