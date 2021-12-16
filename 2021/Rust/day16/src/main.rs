26ae673048a3e1f72bd4d933f96e52fe35b7ba37
Nick Webster <nick@nick.geek.nz>
2021-12-17 02:27:44 +1300

    part 1

--- 2021/Rust/day16/src/main.rs
+++ 2021/Rust/day16/src/main.rs
@@ -1,5 +1,3 @@
-use anyhow::{bail, Result};
-
 fn main() {
     let raw = std::fs::read_to_string("input.txt").unwrap();
     part1(&*raw);
@@ -40,60 +38,85 @@ fn part1(raw: &str) {
     // println!("{:?}", to_dec(&*p.lit_val));
 }
 
-fn parse_packet(binary_str: &str) -> Result<(Packet, usize)> {
+fn parse_packet(binary_str: &str) -> Result<(Packet, usize), usize> {
     let mut p = Packet::default();
+    if binary_str.len() <= 6 {
+        return Err(binary_str.len());
+    }
 
     let mut cur: usize = 0;
-    p.version = to_dec(&binary_str[cur..cur+3]);
+    p.version = to_dec(get_range(binary_str, cur, cur+3));
     cur += 3;
-    p.type_id = to_dec(&binary_str[cur..cur+3]);
+    p.type_id = to_dec(get_range(binary_str, cur, cur+3));
     cur += 3;
 
     if p.type_id == 4 {
         // literal
-        loop {
+        while get_c(cur, binary_str) == '1' {
             cur += 1;
-            if binary_str.as_bytes()[0] == b'0' {
-                break;
-            }
-            p.lit_val.push_str(&binary_str[cur..cur+4]);
+            println!("pushing from {} to {} with len {}", cur, cur + 4, binary_str.len());
+            p.lit_val.push_str(get_range(binary_str, cur, cur+4));
             cur += 4;
+            // if cur > binary_str.len() - 4 {
+            //     break;
+            // }
         }
+        cur += 1;
+        println!("pushing from {} to {} with len {}", cur, cur + 4, binary_str.len());
+        p.lit_val.push_str(get_range(binary_str, cur, cur+4));
+        cur += 4;
     } else {
         // Op
         let len = if binary_str.as_bytes()[cur] == b'1' { 11 } else { 15 };
         cur += 1;
 
-        let sub_len = to_dec(&binary_str[cur..cur+len]);
+        let sub_len = to_dec(get_range(binary_str, cur, cur+len));
         cur += len;
 
         if len == 11 {
             for _ in 0..sub_len {
-                match parse_packet(&binary_str[cur..]) {
+                match parse_packet(get_range(binary_str, cur, binary_str.len())) {
                     Ok((sub, consumed)) => {
                         p.sub_packets.push(sub);
                         cur += consumed;
                     },
-                    Err(_) => {}
+                    Err(consumed) => {
+                        cur += consumed;
+                    }
                 }
             }
         } else {
             let mut max = cur + sub_len;
+            println!("max: {}, len: {}, cur: {}", max, binary_str.len(), cur);
 
-            while cur != max {
-                match parse_packet(&binary_str[cur..max]) {
+            while cur < max {
+                match parse_packet(get_range(binary_str, cur, max)) {
                     Ok((sub, consumed)) => {
                         p.sub_packets.push(sub);
                         cur += consumed;
-                         // -= consumed as isize;
                     },
-                    Err(_) => {}
+                    Err(consumed) => {
+                        cur += consumed
+                    }
                 }
             }
         }
     }
 
     Ok((p, cur))
+}
+
+fn get_c(cur: usize, bin_str: &str) -> char {
+    bin_str.as_bytes()[cur] as char
+}
+
+fn get_range(bin_str: &str, from: usize, to: usize) -> &str {
+    // if to <= bin_str.len() {
+    //
+    // } else {
+    //     &bin_str[from..]
+    // }
+    &bin_str[from..to]
 }
 
 fn count_versions(p: &Packet) -> usize {
