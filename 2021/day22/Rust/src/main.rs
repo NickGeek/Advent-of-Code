7cb2d1505412c96442fa03da906ebbd0d3b416f4
Nick Webster <nick@nick.geek.nz>
2021-12-22 22:49:45 +1300

    I don't know what I'm doing lol

--- 2021/day22/Rust/src/main.rs
+++ 2021/day22/Rust/src/main.rs
@@ -50,50 +50,92 @@ fn main() {
         let from = (ranges[0].0, ranges[1].0, ranges[2].0);
         let to = (ranges[0].1, ranges[1].1, ranges[2].1);
 
+        let mut new_state = state.clone();
         if status == "on" {
-            if let Some(idx) = intersects(&(from, to), &state) {
-                // Expand!
-                let (_, other_f, other_t) = state[idx];
-                let min_x = if from.0 < other_f.0 { from.0 } else { other_f.0 };
-                let max_x = if to.0 > other_t.0 { to.0 } else { other_t.0 };
-                let min_y = if from.1 < other_f.1 { from.1 } else { other_f.1 };
-                let max_y = if to.1 > other_t.1 { to.1 } else { other_t.1 };
-                let min_z = if from.2 < other_f.2 { from.2 } else { other_f.2 };
-                let max_z = if to.2 > other_t.2 { to.2 } else { other_t.2 };
-
-                state[idx] = (true, (min_x, min_y, min_z), (max_x, max_y, max_z));
-            } else {
-                state.push((true, from, to));
-            }
+            add_cuboid((from, to), &mut new_state);
         } else {
-            if let Some(idx) = intersects(&(from, to), &state) {
+            let idxs = intersects(&(from, to), &state);
+            if !idxs.is_empty() {
                 // Expand!
-                let (_, other_f, other_t) = state[idx];
-                let (del_f, del_t) = intersection_pt((from, to), (other_f, other_t));
+                // let mut new_boxes = vec![];
 
-                // Simple case, complete wipe
-                if other_f.0 >= del_f.0 && del_t.0 <= other_t.0 {}
+                for idx in idxs {
+                    let (_, other_f, other_t) = state[idx];
+                    let itx = intersection_pt((from, to), (other_f, other_t));
 
-                let mut new_cuboids = Vec::new();
+                    new_state.remove(idx);
 
-                // state[idx] = (false, (min_x, min_y, min_z), (max_x, max_y, max_z));
+                    // x1 < deletion start
+                    if other_f.0 < (from, to).0.0 {
+                        let mut max = other_t;
+                        max.0 = (from, to).0.0 - 1;
+                        add_cuboid((other_f, max), &mut new_state);
+                    }
+
+                    // y1 < deletion start
+                    if other_f.1 < (from, to).0.1 {
+                        let mut max = other_t;
+                        max.1 = (from, to).0.1 - 1;
+                        add_cuboid((other_f, max), &mut new_state);
+                    }
+
+                    // z1 < deletion start
+                    if other_f.2 < (from, to).0.2 {
+                        let mut max = other_t;
+                        max.2 = (from, to).0.2 - 1;
+                        add_cuboid((other_f, max), &mut new_state);
+                    }
+
+                    // x2 > deletion end
+                    if other_t.0 > (from, to).1.0 {
+                        let mut min = other_f;
+                        min.0 = (from, to).0.0 - 1;
+                        add_cuboid((min, other_t), &mut new_state);
+                    }
+
+                    if other_t.1 > (from, to).0.1 {
+                        let mut min = other_f;
+                        min.1 = (from, to).0.1 - 1;
+                        add_cuboid((min, other_t), &mut new_state);
+                    }
+
+                    if other_t.2 > (from, to).0.2 {
+                        let mut min = other_f;
+                        min.2 = (from, to).0.2 - 1;
+                        add_cuboid((min, other_t), &mut new_state);
+                    }
+                }
             }
-
-            // if let Some(idx) = intersects(&(from, to), &state) {
-            //     let (other_f, other_t) = state[idx];
-            //
-            //     let (del_f, del_t) = intersection_pt((from, to), (other_f, other_t));
-            // }
         }
 
-        // state.push((status == "on", from, to));
+        state = new_state;
     }
     println!("{:?}", state);
 }
 
-fn intersects(range: &(Pos, Pos), state: &Vec<(bool, Pos, Pos)>) -> Option<usize> {
-    state.iter()
-        .position(|other| {
+fn add_cuboid((from, to): (Pos, Pos), state: &mut Vec<(bool, Pos, Pos)>) {
+    let idxs = intersects(&(from, to), &state);
+    if !idxs.is_empty() {
+        for idx in idxs {
+            // Expand!
+            let (_, other_f, other_t) = state[idx];
+            let min_x = if from.0 < other_f.0 { from.0 } else { other_f.0 };
+            let max_x = if to.0 > other_t.0 { to.0 } else { other_t.0 };
+            let min_y = if from.1 < other_f.1 { from.1 } else { other_f.1 };
+            let max_y = if to.1 > other_t.1 { to.1 } else { other_t.1 };
+            let min_z = if from.2 < other_f.2 { from.2 } else { other_f.2 };
+            let max_z = if to.2 > other_t.2 { to.2 } else { other_t.2 };
+
+            state[idx] = (true, (min_x, min_y, min_z), (max_x, max_y, max_z));
+        }
+    } else {
+        state.push((true, from, to));
+    }
+}
+
+fn intersects(range: &(Pos, Pos), state: &Vec<(bool, Pos, Pos)>) -> Vec<usize> {
+    state.iter().enumerate()
+        .filter(|(i, other)| {
             let other = &(other.1, other.2);
             let x = range.0.0 <= other.1.0 && range.1.0 >= other.0.0;
             let y = range.0.1 <= other.1.1 && range.1.1 >= other.0.1;
@@ -101,6 +143,8 @@ fn intersects(range: &(Pos, Pos), state: &Vec<(bool, Pos, Pos)>) -> Option<usize
 
             x && y && z
         })
+        .map(|(i, _)| i)
+        .collect()
 }
 
 fn intersection_pt(range: (Pos, Pos), other: (Pos, Pos)) -> (Pos, Pos) {
@@ -121,14 +165,11 @@ fn intersection_pt(range: (Pos, Pos), other: (Pos, Pos)) -> (Pos, Pos) {
     (min, max)
 }
 
-fn volume(state: &Vec<(bool, Pos, Pos)>) -> u64 {
-    let mut c = 0;
-    for range in state {
-        let (on, (x1, y1, z1), (x2, y2, z2)) = *range;
-
-    }
-
-    c
+fn volume(cuboid: (Pos, Pos)) -> i64 {
+    let length = (cuboid.1.0 - cuboid.0.0).abs();
+    let height = (cuboid.1.1 - cuboid.0.1).abs();
+    let depth = (cuboid.1.2 - cuboid.0.2).abs();
+    length * height * depth
 }
 
 
